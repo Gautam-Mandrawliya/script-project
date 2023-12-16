babe6cb0625592abdceebd809ae70d22868b8fc6
root <root@ip-172-31-39-148.ap-south-1.compute.internal>
2023-12-16 09:58:23 +0000

    Update Script

---
+++ amazon-linux.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Create functions
+function DockerPlugins() { 
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; 
+then
+            echo  "Docker is not installed. Installing Docker..."
+          # Docker installation and start and enable docker service
+            sudo yum update -y
+            sudo amazon-linux-extras install docker
+            sudo yum install -y docker
+            sudo usermod -a -G docker $USER
+            sudo systemctl enable --now docker
+            echo  "Docker is successfully install, configured and services start or enabled at boot time"
+fi
+
+# Check if Docker Compose is installed
+if ! command -v docker-compose &> /dev/null; 
+then
+            echo "Docker Compose is not installed. Installing Docker Compose..."
+          # Docker Compose Installation
+            sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+            sudo chmod +x /usr/local/bin/docker-compose
+          # docker-compose version
+            echo "Docker-Compose plugin is successfully installed."
+fi
+}
+
+function SiteConfigure() {
+echo "Creating WordPress site: example.com"
+
+# Create /etc/hosts entry
+        echo "127.0.0.1   example.com" | sudo tee -a /etc/hosts >/dev/null
+
+# Start the containers
+        sudo docker-compose up -d
+        Status
+        sudo docker container ls
+}
+
+function Status() {
+        until $(curl --output /dev/null --silent --head --fail  http://example.com);
+        do
+                echo "Waiting for the site to be up..."
+                sleep 5
+        done
+                echo "The site example.com is up and healthy. Please open http://example.com in your browser."
+}
+
+function Start() {
+# Additional subcommand: Start the site
+        sudo docker-compose start
+        Status
+        echo "The site example.com has been started."
+}
+
+function Stop(){
+# Additional subcommand: Stop the site
+        sudo docker-compose stop
+        echo "The site example.com has been stopped."
+}
+
+function Remove() {
+# Additional subcommand: Delete the site
+        sudo docker-compose down --volumes
+        sudo sed -i "/example.com/d" /etc/hosts
+        echo "The site example.com  has been deleted from the /etc/hosts file."
+#       rm -rvf docker-compose.yml wp-content
+}
+
+# Execute the program
+if [[ "$1" == "start" ]]
+then
+        Start
+elif [[ "$1" == "stop" ]]
+then
+        Stop
+elif [[ "$1" == "delete" ]]
+then
+        Remove
+else
+        DockerPlugins
+        SiteConfigure
+fi
--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Create functions
-function Install() { 
+function DockerPlugins() { 
 # Check if Docker is installed
 if ! command -v docker &> /dev/null; 
 then
 	    echo  "Docker is not installed. Installing Docker..."
           # Docker installation and start and enable docker service
-	    yum update -y
-	    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
-	    yum install -y docker-ce
-	    systemctl start docker
-	    systemctl enable docker
-	    echo  "Docker is successfully installed and service start and enable at boot time"
+	    sudo yum update -y
+	    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	    sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	    sudo usermod -a -G docker $USER
+	    sudo systemctl enable --now docker
+	    echo  "Docker is successfully install, configured and services start or enabled at boot time"
 fi
 
 # Check if Docker Compose is installed
@@ -20,60 +20,26 @@ if ! command -v docker compose &> /dev/null;
 then
 	    echo "Docker Compose is not installed. Installing Docker Compose..."
 	  # Docker Compose Installation
-	    yum install docker-compose-plugin -y
-    	    docker compose version
-	    echo "Docker Compse is successfully installed."
+	    sudo yum install docker-compose-plugin -y
+	    docker compose version
+	    echo "Docker-Compose plugin is successfully installed."
 fi
+}
 
+function SiteConfigure() {
 echo "Creating WordPress site: example.com"
-# Create docker-compose.yml file
-cat > docker-compose.yml << EOF
-version: '3.9'
-
-services:
-  db:
-    image: mysql:5.7
-    volumes:
-      - db_data:/var/lib/mysql
-    restart: always
-    environment:
-       MYSQL_ROOT_PASSWORD: abhay2018
-       MYSQL_DATABASE: wordpress
-       MYSQL_USER: wordpress
-       MYSQL_PASSWORD: wordpress
-    networks:
-       - wordpress-net
-
-  wordpress:
-    depends_on:
-    - db
-    image: wordpress:latest
-    ports:
-      - "80:80"
-    restart: always
-    volumes:
-      - ./wp-content:/var/www/html/wp-content
-    environment:
-      WORDPRESS_DB_HOST: db:3306
-      WORDPRESS_DB_USER: wordpress
-      WORDPRESS_DB_PASSWORD: wordpress
-      WORDPRESS_DB_NAME: wordpress
-    networks:
-      - wordpress-net
-
-networks:
-  wordpress-net:
-volumes:
-  db_data:
-EOF
 
 # Create /etc/hosts entry
 	echo "127.0.0.1   example.com" | sudo tee -a /etc/hosts >/dev/null
+	
+# Access and add rules in the firewall
+	sudo firewall-cmd --add-port={8080/tcp,80/tcp,3306/tcp} --permanent
+	sudo firewall-cmd --reload
 
 # Start the containers
-	docker compose up -d
+	sudo docker compose up -d
 	Status
-	docker container ls
+	sudo docker container ls
 }
 
 function Status() {
@@ -82,26 +48,25 @@ function Status() {
 		echo "Waiting for the site to be up..."
 		sleep 5
 	done
-		echo "The site example.com is up and healthy."	
-		echo "Please open http://example.com in your browser."
+		echo "The site example.com is up and healthy. Please open http://example.com in your browser."	
 }
 
 function Start() {
 # Additional subcommand: Start the site
-	docker compose start
+	sudo docker compose start
 	Status
 	echo "The site example.com has been started."
 }
 
 function Stop(){
 # Additional subcommand: Stop the site
-	docker compose stop
+	sudo docker compose stop
 	echo "The site example.com has been stopped."
 }
 
 function Remove() {
 # Additional subcommand: Delete the site
-	docker compose down --volumes
+	sudo docker compose down --volumes
 	sudo sed -i "/example.com/d" /etc/hosts
 	echo "The site example.com  has been deleted from the /etc/hosts file."
 #	rm -rvf docker-compose.yml wp-content
@@ -118,5 +83,6 @@ elif [[ "$1" == "delete" ]]
 then
 	Remove
 else
-	Install
+	DockerPlugins
+	SiteConfigure
 fi
